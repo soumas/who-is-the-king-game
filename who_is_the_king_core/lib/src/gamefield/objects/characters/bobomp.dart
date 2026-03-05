bf327b2a4da71d526dd97444c6295a6f061d8410
Creatives <office@soumasoft.com>
2026-03-05 19:02:35 +0100

    some new rebels

---
+++ who_is_the_king_core/lib/src/gamefield/objects/characters/bobomp.dart
@@ -0,0 +1,37 @@
+import 'dart:math';
+
+import 'package:who_is_the_king_core/src/gamefield/objects/base/attack.dart';
+import 'package:who_is_the_king_core/src/gamefield/objects/base/character.dart';
+
+class Bobomp extends Character {
+  @override
+  String get name => 'BOBOMP';
+
+  @override
+  int get hearts => 9;
+
+  @override
+  int get multiAttackCost => 4;
+
+  @override
+  DefaultAttack get defaultAttack => DefaultAttack(maxThrowingDistance:3,
+    damageArea: [
+    const Point(0, 0)
+   ],
+    canPenetrateWalls: true);
+
+  @override
+  MultiAttack get multiAttack => MultiAttack(
+    maxThrowingDistance: 3,
+    damageArea: [
+      const Point(0, 0),
+      const Point(1, 0),
+      const Point(0, 1),
+      const Point(0, -1),
+      const Point(-1, 0),
+    ],
+    canPenetrateWalls: true,
+    maxMoveDistance: 0,
+    minMoveDistance: 0,
+  );
+}
